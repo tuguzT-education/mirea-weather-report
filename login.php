c5134bb866a7f148892bd2389f1d411bf72dc3aa
tuguzT <56771526+tuguzT@users.noreply.github.com>
2021-01-01 15:46:40 +0300

    Initial commit

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='ru'>
+<head>
+	<meta charset='UTF-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<meta name='description' content='Прогноз погоды из любой точки земного шара'>
+	<meta name='author' content='Тугушев Тимур'>
+	<meta name='color-scheme' content='dark light'>
+	<meta name='keywords' content='weather, погода, прогноз погоды'>
+	<link href='styles/main.css' rel='stylesheet' type='text/css'>
+	<title>Вход | Weather Report</title>
+</head>
+<body>
+<header>
+	<h2>Weather Report</h2>
+</header>
+<main>
+
+</main>
+</body>
+</html>
