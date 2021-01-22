8db602459b4c1c415d77232761b325c94653feb9
tuguzT <timurka.tugushev@gmail.com>
2021-01-22 17:11:58 +0300

    Minor changes

--- js/map.js
+++ js/map.js
@@ -1,26 +1,36 @@
-import '/open_layers/ol.js'
+import '/open_layers/ol.js';
 
-const extent = ol.proj.transformExtent([-180, -85, 180, 85], 'EPSG:4326', 'EPSG:3857')
+const extent = ol.proj.transformExtent([-180, -85, 180, 85], 'EPSG:4326', 'EPSG:3857');
 
-const map = new ol.Map({
-	target: 'map',
+const view = new ol.View({
+	center: [0, 0],
+	zoom: 0,
+	extent: extent
+});
+
+const OSMLayer = new ol.layer.Tile({
+	preload: Infinity,
+	source: new ol.source.OSM(),
+	extent: extent
+});
+
+const layerGroup = new ol.layer.Group({
 	layers: [
+		OSMLayer,
 		new ol.layer.Tile({
-			source: new ol.source.OSM(),
-			extent: extent
-		}),
-		new ol.layer.Tile({
+			preload: Infinity,
 			source: new ol.source.XYZ({
-				url:
+				urls:
 					'https://tile.openweathermap.org/map/temp_new/' +
 					'{z}/{x}/{y}.png?appid=746233f13b8578f62fe9dc1730285e03'
 			}),
 			extent: extent
 		})
-	],
-	view: new ol.View({
-		center: [0, 0],
-		zoom: 0,
-		extent: extent
-	})
-})
+	]
+});
+
+const map = new ol.Map({
+	target: 'map',
+	layers: layerGroup,
+	view: view
+});
--- map.php
+++ map.php
@@ -18,8 +18,15 @@ userHeaderHTML();
 <main>
 	<div class="panel full_width padding_1p275 margin_2_vert">
 		<h3 class="margin_0">Интерактивная погодная карта</h3>
-		<div id="map" class="margin_1_top"></div>
-		<script type="module" src="/js/map.js"></script>
+		<div id="map" class="margin_1_top" tabindex="1"></div>
+		<div class="margin_1_top">
+			<label for="map_panel_layer_select" class="margin_1_right">Выберите отображаемый слой</label>
+			<select id="map_panel_layer_select">
+				<option hidden>Слой не выбран...</option>
+				<option>Температура</option>
+			</select>
+		</div>
+		<script type="module" src="/js/map.js" async></script>
 	</div>
 </main>
 <?php
